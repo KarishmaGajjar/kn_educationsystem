3f3b85ea63cc7afa040e5a0725a38a345d008014
unknown <niralidadhaniya2001@gmail.com>
2023-08-25 10:27:30 +0530

    Nirali : header

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import coursera_logo from './coursera_logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <>
+      <div className='container-fluid mt-4'>
+        <div className='row'>
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='col-md-2'>
+                <img src={coursera_logo} height="32px" width="127px" alt=""></img>
+              </div>
+              <div className='col-md-2'>
+                <input type={'button'} value="Explore" name="expore" class="btn text-light" style={{backgroundColor:"#0056d2"}}></input>
+              </div>
+              <div className='col-md-6'>
+                <div className='row'>
+                <div className='col-4'><input type={'text'} value="" class="form-control" placeholder='What do you want?'></input></div>
+                <div className='col-1'><button class="btn text-light fa fa-search" style={{backgroundColor:"#0056d2"}}></button></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='row'>
+              <div className='col-4'>Home</div>
+              <div className='col-4'>About</div>
+              <div className='col-4'>Categories</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
